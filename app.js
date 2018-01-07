a48d297a41330d0fedbdf4218745e61108dcc503
Chris Lesesne <clesesne3@gmail.com>
2018-01-07 12:32:01 -0800

    form elements function/style working except select dropdown

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 'use strict';
 
 $(document).ready(function() {
-  $('#text-error').addClass('color-border');
-  $('<p class="text-color">please enter a valid username</p>').insertAfter('#text-error').addClass('align-right');
+  $('.error').hide();
+
+  $('#text-one').keypress(function(event) {
+    if(event.which == 13) {
+      if(!$(this).val()) {
+        $('.text-one').hide();
+        $('.error').show();
+        $('#text-error').addClass('color-border');
+        $('<p class="text-color error">please enter a valid username</p>').insertAfter('#text-error').addClass('align-right');
+      }
+    }
+  });
+
+  // select dropdown
+  $('#select-box').addClass('sprite-select-up');
+
+  //checkbox
+  $('#checkbox').addClass('sprite-unchecked').on('click', function() {
+    $('#checkbox').toggleClass('sprite-checked');
+  });
+
+  // radio
+  $('#radio').addClass('sprite-radio-clear').on('click', function() {
+    $('#radio').toggleClass('sprite-radio-clicked sprite-radio-clear');
+  });
+
+  // submit button
+  $('#submit').addClass('bkgrd-color');
+  $('#submit').on('click', function() {
+    $('#submit').toggleClass('bkgrd-color-click');
+  });
 });
